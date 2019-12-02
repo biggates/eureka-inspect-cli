3feded435819e595681c2d1aa884bf795bd2c2d2
Xiaoyu Guo <biggates2010@gmail.com>
2019-12-02 16:47:02 +0800

    example setup.py

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import ast
+import re
+
+from setuptools import Command, find_packages, setup
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('eureka_inspect_cli/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+description = 'CLI for quickly inspect nodes registered with eureka server'
+
+install_requirements = [
+    'click >= 7.0',
+    'colorama >= 0.4.1',
+    'requests >= 2.22.0',
+    'pyquery >= 1.4.1',
+]
+
+setup(
+    name='eureka_inspect_cli',
+    author='biggates',
+    author_email='biggates_2010@gmail.com',
+    version=version,
+    url='https://github.com/biggates/eureka-inspect-cli',
+    packages=find_packages(),
+    description=description,
+    long_description=description,
+    install_requires=install_requirements,
+    entry_points={
+        'console_scripts': ['eureka_inspect = eureka_inspect_cli.main:cli']
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
